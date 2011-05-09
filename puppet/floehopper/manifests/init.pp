e9be8c023e6ba871bbf0a5b7e69bf63849bbd9bf
James Mead <james@floehopper.org>
2011-05-09 13:14:38 +0100

    Fixed syntax error.

--- puppet/floehopper/manifests/init.pp
+++ puppet/floehopper/manifests/init.pp
@@ -15,7 +15,8 @@ class floehopper {
     floehopper::user {"james@floehopper.org":
       user => "jamesmead",
       key => "AAAAB3NzaC1yc2EAAAABIwAAAQEA2sgFVWa22MUpTC3d7rn04qhzLRc5CwwuBLGp3dm3Yg5mFpq2dQLMlWZN/uK4SK1DC8fEDCbnaq4iGUk1VAUt6tN8UxrR9P5yrxSWTLLwanpNb/G6I4sRbzJaowhEchPLvoTWpege6hX6xGzHJRspODxT+Dhhrx8RIT5ZU19IlMEzSpYXNtBk5P+poj3AwC4gBwGhtC/B/YltM6Cxpi6Tp1Jb7vilRW08GU8EkmiBhs45QF2wWV2+OMinW7QIww7c6NdrbIDWw9+FBGOUHRlyPpXMCuz8B1zgplzoFLlO13r2fPDx2pB8WodMkZie28s1tfLB4gKIHVWbf9YYyLQeTQ=="
-    },
+    }
+
     floehopper::user {"hannah.smithson@durham.ac.uk":
       user => "hannahsmithson",
       key => "AAAAB3NzaC1yc2EAAAABIwAAAQEAvSVPEv/UNEMrdZ1FAWX5mSu8kL6W21TX81T4+2fAC6cfSOyzFRSg1H99fpeQJtXQld6VmjUoj4jrkiXRhu+d4BFCLwaYFCqFAD0WsGxLYfKDlonhUSHDZhz5yR9qic42VIqtWKzO1KY+P8I3Y2eYYgQiDVZHGIHx6Am2ZIBOw1vigrnOrhi361p0EFlyLY1Y2Kkg/x94NAUI+74mRavUj1DmxEz4jBPmj7XAWVQVQG5c9KPNbV9BgPh1ZiQlrg3mhiD5W7SFEqaKywiRMjFrLpzXq5a2hg9QqHRjIK7Jh2+8EjGEbZKUWvByBLmk3zLAUa54Nmk0I5zJijGY9kfjAw=="
